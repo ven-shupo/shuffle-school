9956341024cddb9ae81ee9714fda66441a34cca2
opushnev <opushnev.ia@phystech.edu>
2023-11-05 17:04:54 +0300

    comit

--- components/preview.js
+++ components/preview.js
@@ -1,19 +1,28 @@
-import React, {useState} from 'react'
-import Airtable from 'airtable';
-import styles from '../styles/Home.module.css';
+import React, {useState, useEffect} from 'react'
 import {useTelegramWeb} from "../lib/telegramWeb";
-import $ from 'jquery'; 
 
 
 function Preview () {
-
-  const tg = useTelegramWeb();
-  tg.MainButton.setParams({text: 'Закрыть', is_visible: true}).onClick(() => {
-    tg.close()
-  });
+//   const tg = useTelegramWeb();
+//   tg.MainButton.setParams({text: 'Закрыть', is_visible: true}).onClick(() => {
+//     tg.close()
+//   });
   const [lessons, setLessons] = useState(0)
-  var base = new Airtable({ apiKey: 'patjwWYpmH9i2vk6n.ac5d32a4790e27c8cf63bc7763bb4ad5fd8aef3177c872447551cd39937e7f25' }).base('appYXVwx6zw8wmTW3');
-  console.log(base('members').select())
+  useEffect(() => {
+    const requestOptions = {
+        method: 'GET',
+        headers: { "Authorization": "Bearer patjwWYpmH9i2vk6n.ac5d32a4790e27c8cf63bc7763bb4ad5fd8aef3177c872447551cd39937e7f25"},
+    };
+    fetch('https://api.airtable.com/v0/appYXVwx6zw8wmTW3/members?filterByFormula=email%3D7', requestOptions)
+       .then((response) => response.json())
+       .then((data) => {
+          console.log(data);
+        //   setPosts(data);
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+ }, []);
   return (
     <div
       style={{backgroundColor: 'var(--tg-theme-bg-color)'}}
