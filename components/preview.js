d07c50418d73bb860a81ffbf188f8e1e91cc5002
opushnev <opushnev.ia@phystech.edu>
2024-01-28 16:25:26 +0300

    new url

--- components/preview.js
+++ components/preview.js
@@ -12,14 +12,19 @@ function Preview () {
   console.log("tg.initDataUnsafe", tg.initDataUnsafe)
   console.log("tg.initDataUnsafe.user", tg.initDataUnsafe.user)
   console.log("tg.initDataUnsafe.user.username", tg.initDataUnsafe.user.username)
-  const tgUserName =  tg.initDataUnsafe.user.username;
+  const tgUserName = tg.initDataUnsafe.user.username;
   const [lessons, setLessons] = useState();
+
+  var url = new URL("https://api.airtable.com/v0/appXfAFgufLXTHPVr/dancer");
+  var params = {filterByFormula:tgUserName};
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
   useEffect(() => {
     const requestOptions = {
         method: 'GET',
         headers: { "Authorization": "Bearer pattpUkpI0kiExoi9.e98cfe85447f4a5d49fbd63d0f59baa57121f7578e207036c666c8cb0329eeb9"},
     };
-    fetch('https://api.airtable.com/v0/appXfAFgufLXTHPVr/dancer?filterByFormula=tg%3D' + tgUserName, requestOptions)
+    fetch(url, requestOptions)
        .then((response) => response.json())
        .then((data) => {
             setLessons(data.records[0].fields.classes_left);
@@ -27,7 +32,7 @@ function Preview () {
        .catch((err) => {
           console.log(err.message);
        });
- }, []);
+  }, []);
   return (
     <div
         className={styles.preview}
