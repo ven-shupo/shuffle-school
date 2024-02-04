c1d13a3788ed1f4dc9d35af40e46bc24c4c3d5c8
opushnev <opushnev.ia@phystech.edu>
2024-02-04 18:07:40 +0300

    fix

--- components/preview.js
+++ components/preview.js
@@ -4,7 +4,8 @@ import styles from '../styles/Home.module.css';
 
 
 function choosePlural(count) {
-    let words = ['занятие', 'занятия', 'занятий'];
+    var cases = [2, 0, 1, 1, 1, 2];
+    var words = ['занятие', 'занятия', 'занятий'];
     return words[ (count % 100 > 4 && count % 100 < 20) ? 2 : cases[ Math.min(count % 10, 5)] ];
 }
 
